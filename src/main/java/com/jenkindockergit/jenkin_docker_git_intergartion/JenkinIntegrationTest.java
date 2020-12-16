16123753b8ff2ff4786795dca0b0ec8dee8e5abf
saravanaprabuA <saravanaprabu.a@gmail.com>
2020-12-16 14:46:57 +0530

    New java file added

---
+++ src/main/java/com/jenkindockergit/jenkin_docker_git_intergartion/JenkinIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.jenkindockergit.jenkin_docker_git_intergartion;
+
+public class JenkinIntegrationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Jenkin Git Integration Test");
+	}
+
+}
