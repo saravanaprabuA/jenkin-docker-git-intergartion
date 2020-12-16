4a98fa30d9f77fb978ce64f60f97cad87ca420d5
saravanaprabuA <saravanaprabu.a@gmail.com>
2020-12-16 14:06:59 +0530

    first commit

---
+++ src/main/java/com/jenkindockergit/jenkin_docker_git_intergartion/App.java
@@ -0,0 +1,13 @@
+package com.jenkindockergit.jenkin_docker_git_intergartion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
